2264997c0b075d179d36d01b9a56541748d3b687
risa0925 <g14956wr@gm.tsuda.ac.jp>
2017-07-05 17:09:49 +0900

    first commit

--- python/main.py
+++ python/main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+# coding: utf-8
 import webapp2
 
+form_HTML = """
+    <form action="/output" method="post">
+        <input type="text" name="a"><br>
+        <input type="text" name="b"><br>
+        <input type="submit">
+    </form>"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('こんにちは！')
+        self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+        self.response.write('<title>パタトクカシーー</title>')
+        self.response.write(form_HTML)
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+class outputPage(webapp2.RequestHandler):
+    def post(self):
+        a_list = list(self.request.get('a'))
+        b_list = list(self.request.get('b'))
+        output_list = []
+
+        for i in range(len(a_list)):
+            output_list.append(a_list[i])
+            output_list.append(b_list[i])
+        outputWord = "".join(output_list)
+
+        self.response.write(outputWord)
+        self.response.write(form_HTML)
+
+app = webapp2.WSGIApplication([('/', MainPage),('/output',outputPage)], debug=True)
